fea3f25a48944ef3e04de0192d763031957d93c9
patrickfinnigan <patrickfinnigan94@gmail.com>
2019-06-08 18:29:26 -0700

    getting the hang of it

---
+++ spotlight/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Green from "./components/Green/Green";
+
+function App() {
+  return (
+      <Green />
+  );
+}
+
+export default App;
---
+++ spotlight/src/components/Green/Green.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Green.css";
+
+const Green = () => (
+    <div>
+    <h1 className="green">Green Light</h1>
+    </div>
+)
+
+export default Green;
